796b88db3e2c88915befda9151c5d9f8a0e5df80
usehotkey <igolopolosov@gmail.com>
2019-02-05 22:50:01 +0000

    [feature] Add "x days to Brexit" to the main screen

--- dashboard/src/app/app.component.spec.ts
+++ dashboard/src/app/app.component.spec.ts
@@ -16,16 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dashboard'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Home');
-  });
-
-  it('should render title in a h1 tag', () => {
+  it('should render Brexit message in title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome Home!');
+    expect(compiled.querySelector('h1').textContent).toContain(`It's ??? days to Brexit :(`);
   });
 });
--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+const day = 1000 * 60 * 60 * 24;
+const roundTo = (value, to) => {
+  const power = Math.pow(10, to)
+  return Math.round(value * power) / power
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'Home';
+  daysToBrexit: string|number = '???'
+
+  constructor() {
+    this.setDateUpdate()
+  }
+
+  setDateUpdate() {
+    setInterval(() => {
+      const diff = <any>new Date('2019/03/29') - <any>new Date();
+      this.daysToBrexit = roundTo(diff / day, 3)
+    }, 1000)
+  }
 }
