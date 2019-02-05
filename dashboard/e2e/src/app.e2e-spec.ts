1b4a600d8b4551017983343cf6538faeeebd5691
usehotkey <igolopolosov@gmail.com>
2019-02-05 22:20:52 +0000

    [tests] Fix tests for title on main page

--- dashboard/e2e/src/app.e2e-spec.ts
+++ dashboard/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to dashboard!');
+    expect(page.getTitleText()).toEqual('Welcome Home!');
   });
 
   afterEach(async () => {
